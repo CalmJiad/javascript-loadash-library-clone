e05a17d8bcd867b1bd857d1bfb9b9510ec8add6e
calmjiad <unlucky4246@gmail.com>
2024-01-07 22:27:25 +0100

    Created compact function

---
+++ array-functions/compact.js
@@ -0,0 +1,19 @@
+const arrayCompact = (arr) => {
+  const resArr = arr.reduce((acc, curr) => {
+    if (
+      curr !== false &&
+      curr !== null &&
+      curr !== 0 &&
+      curr !== undefined &&
+      curr !== "" &&
+      !Number.isNaN(curr)
+    ) {
+      acc.push(curr);
+    }
+    return acc;
+  }, []);
+
+  console.log(resArr);
+};
+
+arrayCompact([0, 1, false, 2, "", NaN, 3, 4, 5]);
